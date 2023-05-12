f8f4b7aba3615af824ae2dc0223d6f15ec8705d9
lui-lobo <luizdwolf@hotmail.com>
2023-05-12 18:39:04 -0300

    iniciando a instancia do desafio dev

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Entrar</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;400;500;600;700" rel="stylesheet">
+
+        <!-- Bootstrap Css -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" />
+
+        <!-- Css Padrão -->
+        <link rel="stylesheet" href="/css/global.css"/>
+        <link rel="stylesheet" href="/css/AuthPagsCss/auth.css"/>
+  
+
+    </head>
+    <body class="antialiased">
+    <!--Criando o header da autenticação-->
+       <header class="container-fluid">
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid d-flex align-items-center">
+                    <a class="navbar-brand" href="#">
+                        <span class="d-flex align-items-center">
+                            <img src="/img/box.svg" class="logo" alt="Reserva Web">
+                            <h1 class="fs-2">Reserva Web</h1>
+                        </span>
+                    </a>
+                </div>
+            </nav>
+       </header>
+      
+    </body>
+</html>
